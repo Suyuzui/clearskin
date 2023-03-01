e5f4be21167e1341921100e3f6e06b86b4ebd4c3
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2023-03-01 22:52:15 +0900

    edit
    htmlを作って表示しようとしていました

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clearskin import views as skin_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', skin_views.index, name="home")
 ]
