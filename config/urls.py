82c453b5c14b814ef5cf4abb7fe2051cbcf09044
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2023-03-14 10:07:08 +0900

    create new app
    ログイン機能を作っている

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from clearskin import views as views_py
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clear', views_py.home, name="home"),
+    path('authtest/', include('authtest.urls')),
 ]
