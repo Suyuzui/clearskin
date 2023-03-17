e6d1446f18fbf262c5fdfbf1516651d090d58fb5
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2023-03-17 22:45:33 +0900

    import openai

--- clearskin/tests.py
+++ clearskin/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+import openai
+
+APK_KEY = ""
+
+def chat_gpt(prompt):
+    openai.api_key = APK_KEY #API KEYをセット
+    openai.Model.list() #OpenAIのインスタンスを生成
+  
+    #APIを使ってリクエストを投げる
+    response = openai.Completion.create(
+    model = "text-davinci-003",
+    prompt= prompt,
+    temperature=0,
+    max_tokens=500,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+
+    )
+    return response
