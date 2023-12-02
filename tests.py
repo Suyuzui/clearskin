6ceb5e6dc61163c16449ff17c41696ecf625a0af
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2023-12-02 19:30:10 +0900

    updates
    pipコマンドが使えるようにした。venv2内で開けるようにはなったけど、APIの制限がかかって文字の返答が返ってこない。

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ def chat_gpt(prompt):
     model = "text-davinci-003",
     prompt= prompt,
     temperature=0,
-    max_tokens=300,
+    max_tokens=100,
     top_p=1.0,
     frequency_penalty=0.0,
     presence_penalty=0.0
